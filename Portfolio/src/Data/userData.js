93f719bb10394333843a84efa8d5ab0402fc65a2
abhii47 <150163008+abhii47@users.noreply.github.com>
2025-04-20 12:28:54 +0530

    Create A userData for Storing data in json Format

---
+++ Portfolio/src/Data/userData.js
@@ -0,0 +1,53 @@
+export const projects = [
+    {
+      title: 'KickCraze',
+      description:
+        'An e-commerce website for sneakers, built with React.js and Tailwind CSS. Features responsive UI, product listings.',
+      live: 'https://kickcraze-abhi.netlify.app/',
+      code: 'https://github.com/abhii47/KickCraze',
+      image: '/assets/kickcraze.png',// Add this image to your public/assets folder
+      languages:[
+        'React.js',
+        'JavaScript',
+        'DaisyUI',
+        'Tailwind CSS',
+        'Framer-Motion',
+        
+      ],
+      active: true,
+    },
+    {
+      title: 'Anime_cast',
+      description:
+        'An anime Character Details concept site using HTML, CSS, and JavaScript. Includes genre filters and a smooth interface.',
+      live: 'https://anime-cast.netlify.app/',
+      code: 'https://github.com/abhii47/Anime_cast',
+      image: '/assets/animecast.png', // Add this image to your public/assets folder
+      languages:[
+        'HTML',
+        'CSS',
+      ],
+      active: false,
+    },
+  ];
+
+export const certifications = [
+    {
+      title: 'Java Servlet Basics and JSP 101',
+      issuer: 'Simplilearn',
+      link: 'https://simpli-web.app.link/e/NZQ9ccKACSb',
+      date: '15 Feb 2025',
+    },
+    {
+      title: 'Introduction to Selenium',
+      issuer: 'Simplilearn',
+      link: 'https://simpli-web.app.link/e/H7NRFCWACSb',
+      date: '5 April 2025',
+    },
+    {
+      title: 'Information Technology Specialist in HTML and CSS',
+      issuer: 'Certiport',
+      link: 'https://www.certiport.com/portal/Pages/PrintTranscriptInfo.aspx?action=Cert&format=pdf&id=468&psize=1',
+      date: '24 March 2025',
+    },
+  ];
